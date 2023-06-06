d4071f4b0aee549aa1542f850385adcecb1b7087
이예린 <yerinlee@iyelin-ui-MacBookPro.local>
2023-06-06 22:12:06 +0900

    feat: mixin

---  main.dart
+++  main.dart
@@ -1,41 +1,44 @@
-class Human {
-  final String name;
-  Human({required this.name});
+//mixin : 생성자가 없는 클래스
+//클래스에 프로퍼티들을 추가할때 사용
 
-  void sayHello() {
-    print("Hi my name is $name");
+// 생성자를 가지고 있지 않음
+mixin Strong {
+  final double strengthLevel = 1500.99;
+}
+
+mixin QuickRunner {
+  void runQuick() {
+    print('runnnnnnn');
   }
+}
+
+mixin Tall {
+  final double height = 1.99;
 }
 
 enum Team { blue, red }
 
-class Player extends Human {
+// 다른 클래스의 프로퍼티와 메소드를 긁어다오기
+// 상속 받을 필요가 없음
+// Mixin => 재사용의 의미
+class Player with Strong, QuickRunner, Tall {
   final Team team;
 
-  // 데이터를 받은 후 team은 현재 클래스에 정의해주고, name은 super 클래스에 전달함 (부모 클래스에 전달함)
   Player({
     required this.team,
-    required String name, // Human으로 전달
-  }) : super(name: name); //: super(name); 이런 방식으로 바로 건네줘도 무방
-  /** 
-   * name을 super 생성자에 전달
-   * super 라는 키워드를 통해 (확장을 한) 부모 클래스와 상호작용할 수 있게 해줌
-  */
-
-  // human에서 온 sayHello를 직접 만든 메소드로 override함
-  @override
-  void sayHello() {
-    // 상속한 부모 클래스의 프로퍼티에 접근하게 하거나, 메소드를 호출할 수 있게 함
-    super.sayHello();
-    print('and i play for ${team}');
-  }
+  });
 }
-
-class Coach {}
 
 void main() {
-  // player 생성자 함수에 있는 name을 human으로 전달해주고 있음
-  var player = Player(team: Team.red, name: 'nico');
+  var player = Player(
+    team: Team.red,
+  );
 
-  player.sayHello();
+  player.runQuick();
 }
+
+
+/**
+ * extend: 확장한 클래스는 부모 클래스가 되고, 자식 클래스는 부모 클래스를 super를 통해서 접근할 수 있고 그 순간 부모 클래스의 인스턴스가 됨
+ * mixin: 생성자가 없는 클래스. with라는 키워드를 사용해서, 단순히 Mixin 내부의 프로퍼티와 메소드를 가져오는 것 뿐. 부모 클래스가 되거나 하지는 않음
+ */
