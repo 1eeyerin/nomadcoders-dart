db73141c11b061df9378cc46646a6e95976ba385
이예린 <yerinlee@iyelin-ui-MacBookPro.local>
2023-06-06 20:09:34 +0900

    feat: named constructors

---  main.dart
+++  main.dart
@@ -12,17 +12,39 @@ class Player {
     required this.age,
   });
 
+/**
+ * 새로운 메서드 만듬
+ */
+  Player.createBluePlayer({
+    required String name,
+    required int age,
+  })  : // : player class 초기화
+        this.age = age,
+        this.name = name,
+        this.team = 'blue',
+        this.xp = 0;
+
+  Player.createRedPlayer(String name, int age)
+      : this.age = age,
+        this.name = name,
+        this.team = 'red',
+        this.xp = 0;
+
   void sayHello() {
-    print('hi my name is $name, my xp ${xp + 1}');
+    print('hi my name is $name, age $age, team $team, xp ${xp + 1}');
   }
 }
 
 void main() {
-  var player = Player(
-    name: 'yerin',
-    xp: 1500,
-    team: 'blue',
+  var bluePlayer = Player.createBluePlayer(
+    name: 'yerin-blue',
     age: 21,
   );
-  player.sayHello();
+  var redPlayer = Player.createRedPlayer(
+    'yerin-red',
+    21,
+  );
+
+  bluePlayer.sayHello();
+  redPlayer.sayHello();
 }
