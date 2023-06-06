c750bf7fb86e1320b009251de5271dccd22f730b
이예린 <yerinlee@iyelin-ui-MacBookPro.local>
2023-06-06 19:57:42 +0900

    feat: named constructor parameters

---  main.dart
+++  main.dart
@@ -1,26 +1,16 @@
-/**
- *  
- * class Player {
- * late final String name;
- * late int xp; 
- * constructor method의 이름은 class 이름과 같아야함
- * Player(String name, int xp) {
- *   this.name = name;
- *  this.xp = xp;
- * }
- * 
- * void sayHello() {
- *   print('hi my name is $name, my xp ${xp + 1}');
- * }
- * }
-*/
-
 class Player {
   final String name;
-  int xp;
+  int xp, age;
+  String team;
 
   // constructor: 전달된 값 초기화
-  Player(this.name, this.xp);
+  // named constructor parameter
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+    required this.age,
+  });
 
   void sayHello() {
     print('hi my name is $name, my xp ${xp + 1}');
@@ -28,8 +18,11 @@ class Player {
 }
 
 void main() {
-  var player = Player('yerin', 1500);
-  var player2 = Player('yerin1', 100);
+  var player = Player(
+    name: 'yerin',
+    xp: 1500,
+    team: 'blue',
+    age: 21,
+  );
   player.sayHello();
-  player2.sayHello();
 }
