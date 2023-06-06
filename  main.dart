fb9b6ada1f4d409fad8673149c5e031adb70d944
이예린 <yerinlee@iyelin-ui-MacBookPro.local>
2023-06-06 17:37:31 +0900

    feat: null operator

---  main.dart
+++  main.dart
@@ -1,41 +1,24 @@
-//void: 아무것도 리턴하지 않는 함수
-void sayHello(String name) {
-  print("Hello $name nice to meet you");
+// QQ operator ?? ?=
+String capitalizeName(String? name) {
+  if (name != null) {
+    return name.toUpperCase();
+  }
+
+  return 'ANON';
 }
 
-//string을 리턴하는 함수
-String sayHello2(String name) {
-  return "Hello $name nice to meet you";
-}
+String capitalizeName1(String? name) =>
+    name != null ? name.toUpperCase() : 'ANON';
 
-// => fat arrow syntax
-String sayHello3(String name) => "Hello $name nice to meet you";
-
-num plus(num a, num b) => a + b;
-
-// 파라미터 기본값
-String sayHello4({
-  String name = '',
-  int age = 27,
-  String country = 'korea',
-}) {
-  return "Hello $name, you re $age, and you come from $country";
-}
-
-// 파라미터 필수값
-String sayHello5({
-  required String name,
-  required int age,
-  required String country,
-}) {
-  return "Hello $name, you re $age, and you come from $country";
-}
-
-// 파라미터 옵션값
-String sayHello6(String name, int age, [String? country = 'cuba']) {
-  return "Hello $name, you re $age, and you come from $country";
-}
+String capitalizeName2(String? name) => name?.toUpperCase() ?? 'ANON';
 
 void main() {
-  print(sayHello6('yr', 27));
+  print(capitalizeName('yr'));
+  print(capitalizeName(null));
+
+  String? name;
+  //만약 name이 널이면 기본값 할당
+  name ??= 'nico';
+  name = null; // 널이여야 아래 변수에 할당할 수 있으니
+  name ??= 'yr';
 }
