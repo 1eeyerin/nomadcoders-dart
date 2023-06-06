e657e62e59df9085177aa9e960949ab07858acd8
이예린 <yerinlee@iyelin-ui-MacBookPro.local>
2023-06-06 17:22:56 +0900

    feat: 함수

---  main.dart
+++  main.dart
@@ -1,6 +1,41 @@
+//void: 아무것도 리턴하지 않는 함수
+void sayHello(String name) {
+  print("Hello $name nice to meet you");
+}
+
+//string을 리턴하는 함수
+String sayHello2(String name) {
+  return "Hello $name nice to meet you";
+}
+
+// => fat arrow syntax
+String sayHello3(String name) => "Hello $name nice to meet you";
+
+num plus(num a, num b) => a + b;
+
+// 파라미터 기본값
+String sayHello4({
+  String name = '',
+  int age = 27,
+  String country = 'korea',
+}) {
+  return "Hello $name, you re $age, and you come from $country";
+}
+
+// 파라미터 필수값
+String sayHello5({
+  required String name,
+  required int age,
+  required String country,
+}) {
+  return "Hello $name, you re $age, and you come from $country";
+}
+
+// 파라미터 옵션값
+String sayHello6(String name, int age, [String? country = 'cuba']) {
+  return "Hello $name, you re $age, and you come from $country";
+}
+
 void main() {
-  //Sets
-  var numbers = {1, 2, 3, 4};
-  Set<int> numbers2 = {1, 2, 3, 4};
-  //Set은 순서가 있다, List랑 같지만 모든 요소가 유니크 해야한다
+  print(sayHello6('yr', 27));
 }
