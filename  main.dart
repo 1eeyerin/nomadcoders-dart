a6383b0c100a1315b10ea6ff1767d4e42ad4fc86
이예린 <yerinlee@iyelin-ui-MacBookPro.local>
2023-06-06 17:56:04 +0900

    feat: alias

---  main.dart
+++  main.dart
@@ -1,24 +1,34 @@
-// QQ operator ?? ?=
-String capitalizeName(String? name) {
-  if (name != null) {
-    return name.toUpperCase();
-  }
+/** 
+ * typedef
+ * 자료형이 헷갈릴때 도움이 되는 alias
+ * 자료형에 alias를 붙일 수 있게 함
+*/
 
-  return 'ANON';
+typedef ListOfInts = List<int>;
+typedef UserInfo = Map<String, String>;
+
+//숫자 배열을 반환하는 함수(파라미터값도 숫자배열로 들어온다)
+List<int> reverseListOfNumbers(List<int> list) {
+  var reversed = list.reversed;
+  //List를 리버스하면 리스트랑은 조금 다른 iterable이 되어서 다시 list로 변환해주어야함
+  return reversed.toList();
 }
 
-String capitalizeName1(String? name) =>
-    name != null ? name.toUpperCase() : 'ANON';
+//숫자 배열을 반환하는 함수(파라미터값도 숫자배열로 들어온다)
+ListOfInts reverseListOfNumbers1(ListOfInts list) {
+  var reversed = list.reversed;
+  //List를 리버스하면 리스트랑은 조금 다른 iterable이 되어서 다시 list로 변환해주어야함
+  return reversed.toList();
+}
 
-String capitalizeName2(String? name) => name?.toUpperCase() ?? 'ANON';
+// 구조화된 data의 형태를 지정하고 싶다면 class를 사용해야 한다
+String sayHi(UserInfo userInfo) {
+  return "Hi ${userInfo['name']}";
+}
 
 void main() {
-  print(capitalizeName('yr'));
-  print(capitalizeName(null));
+  var reverse = reverseListOfNumbers1([1, 2, 3]);
+  print(reverse);
 
-  String? name;
-  //만약 name이 널이면 기본값 할당
-  name ??= 'nico';
-  name = null; // 널이여야 아래 변수에 할당할 수 있으니
-  name ??= 'yr';
+  print(sayHi({"name": "nico"}));
 }
