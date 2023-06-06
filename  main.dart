209404d94523093ad93e7796b2968ad28225e85e
이예린 <yerinlee@iyelin-ui-MacBookPro.local>
2023-06-06 21:54:05 +0900

    feat: inheritance

---  main.dart
+++  main.dart
@@ -1,53 +1,41 @@
-// 추상화 클래스 (abstract class)로 객체를 생성할 수 없다
-// 어떤 청사진을 가지고 있어야하는지 정의해줌
-// 수많은 청사진에 메소드의 이름과 반환 타입만 정해서 정의 가능
-abstract class Human {
-  void walk();
-}
-
-enum Team { red, blue }
-
-enum XPLevel { beginner, medium, pro }
-
-class Coach extends Human {
-  void walk() {
-    print('the coach is walking');
-  }
-}
-
-class Player extends Human {
-  String name;
-  XPLevel xp;
-  int age;
-  Team team;
-
-  Player({
-    required this.name,
-    required this.xp,
-    required this.age,
-    required this.team,
-  });
-
-  void walk() {
-    print('im walk');
-  }
+class Human {
+  final String name;
+  Human({required this.name});
 
   void sayHello() {
-    print('hi my name is $name, age $age, team $team, xp $xp');
+    print("Hi my name is $name");
   }
 }
 
-void main() {
-  // 값을 수정할때 1..
-  var nico = Player(name: "nico", xp: XPLevel.pro, age: 22, team: Team.red)
-    ..name = 'lala' // nico.name 하는 것과 같음 ;;
-    ..xp = XPLevel.beginner
-    ..team = Team.blue;
+enum Team { blue, red }
 
-  // 값을 수정할때 2..
-  var potato = nico
-    ..name = 'lala' // nico.name 하는 것과 같음 ;;
-    ..xp = XPLevel.medium
-    ..team = Team.red
-    ..sayHello();
+class Player extends Human {
+  final Team team;
+
+  // 데이터를 받은 후 team은 현재 클래스에 정의해주고, name은 super 클래스에 전달함 (부모 클래스에 전달함)
+  Player({
+    required this.team,
+    required String name, // Human으로 전달
+  }) : super(name: name); //: super(name); 이런 방식으로 바로 건네줘도 무방
+  /** 
+   * name을 super 생성자에 전달
+   * super 라는 키워드를 통해 (확장을 한) 부모 클래스와 상호작용할 수 있게 해줌
+  */
+
+  // human에서 온 sayHello를 직접 만든 메소드로 override함
+  @override
+  void sayHello() {
+    // 상속한 부모 클래스의 프로퍼티에 접근하게 하거나, 메소드를 호출할 수 있게 함
+    super.sayHello();
+    print('and i play for ${team}');
+  }
+}
+
+class Coach {}
+
+void main() {
+  // player 생성자 함수에 있는 name을 human으로 전달해주고 있음
+  var player = Player(team: Team.red, name: 'nico');
+
+  player.sayHello();
 }
